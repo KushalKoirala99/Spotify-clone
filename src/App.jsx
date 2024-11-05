a5796063913f795127f3d99e90307d5f70627a5b
kushal <kushalkoirala58@gmail.com>
2024-11-05 12:13:42 +0545

    removed teh code for refreshToken

--- src/App.jsx
+++ src/App.jsx
@@ -2,66 +2,31 @@ import { useEffect, useState } from "react";
 import Login from "./components/Login";
 import {
   getTokenFromUrl,
-  refreshAccessToken,
 } from "./components/spotify-token";
 import MainContent from "./components/MainContent";
-import { getUserData } from "./components/spotify-data.service";
 
 function App() {
   const [token, setToken] = useState(
     sessionStorage.getItem("spotify_token") || null
   );
-  const [refreshToken, setRefreshToken] = useState(
-    sessionStorage.getItem("spotify_refresh_token") || null
-  );
-  const [expiresIn] = useState(3600);
+
 
   useEffect(() => {
     //retrieve token from Url
     const hash = getTokenFromUrl();
     window.location.hash = " "; //clear the hash to clean up the url
     const _token = hash.access_token;
-    const _refreshToken = hash.refresh_token;
 
     if (_token) {
       setToken(_token);
       sessionStorage.setItem("spotify_token", _token);
     }
 
-    if (_refreshToken) {
-      setRefreshToken(_refreshToken);
-      sessionStorage.setItem("spotify_refresh_token", _refreshToken);
-    }
 
-    if (_token && _refreshToken) {
-      const refreshInterval = setTimeout(async () => {
-        try {
-          const newAccessToken = await refreshAccessToken(refreshToken);
-          setToken(newAccessToken);
-          sessionStorage.setItem("spotify_token", newAccessToken);
-        } catch (error) {
-          console.log("Failed to refresh access token:", error);
-        }
-      }, (expiresIn - 300) * 1000); // Refresh 5 minutes before expiration
+  }, [token]);
 
-      return () => clearTimeout(refreshInterval);
-    }
-  }, [token, refreshToken, expiresIn]);
 
-  useEffect(() => {
-    if (token) {
-      userData();
-    }
-  }, );
-
-  async function userData() {
-    try {
-      const res = await getUserData(token);
-      // console.log(res);
-    } catch (e) {
-      console.log("Failed to get user data:", e);
-    }
-  }
+  
 
   return (
     <>
--- src/components/spotify-token.js
+++ src/components/spotify-token.js
@@ -1,10 +1,8 @@
-import axios from "axios";
 
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 const redirectUrl = "http://localhost:5173/";
 const clientId = "58b9a99f4b4146ffb2e52326dc17c065";
-const clientSecret = 'e2ac62a9e6a14fe2b460377dd270d571';
 
 
 const scopes = [
@@ -32,35 +30,4 @@ export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${re
   "%20"
 )}&response_type=token&show_dialog=true`;
 
-
-
-// refreshing the token
-
-export async function refreshAccessToken(refreshToken) {
-  const url = 'https://accounts/spotify.com/api/token';
-  
-  
-  // data to be sent
-  const data = new URLSearchParams();
-  data.append('grant_type',"refresh_token");
-  data.append('refresh_token',refreshToken);
-
-
-  //header
-  const headers= {
-    'content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': `Basic ${btoa(`${clientId}:${clientSecret}`)}` 
-  };
-
-  try {
-    const response = await axios.post(url,data,{ headers });
-    console.log('New Access TOken:' , response.data.access_token);
-    return response.data.access_token; // returns new access token
-  }
-
-  catch(e){
-    console.log('error refreshing access token:', e);
-    throw e;
-  }
-}
 
