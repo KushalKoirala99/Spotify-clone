d11a8c8bea01911a6182756ccf76d35fc4bc5ae0
kushal <kushalkoirala58@gmail.com>
2024-11-15 16:06:50 +0545

    minor css fiexed in sidebar and sidbaritem component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,14 +6,14 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
     <>
       <div
         className={`hidden md:block lg:block border border-black  bg-spotify-sidebar w-80 rounded-xl bg text-gray-500 transition-all duration-300 ease-in-out ${
-          isOpen ? "max-w-80" : "max-w-20"
+          isOpen ? "max-w-80" : "max-w-20 "
         }`}
       >
         {/* logo for  library and toggle button */}
         <button onClick={toggleSidebar}>
           <div
             className={`flex p-2  hover:text-white ${
-              isOpen ? `mb-3 m-4` : `m-4`
+              isOpen ? `mb-3 m-4` : `mx-3`
             }`}
           >
             <div className="cursor-pointer ">
@@ -31,11 +31,7 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
         </button>
 
         {/* playlist starts from here */}
-        <div
-          className={`overflow-x-hidden ${
-            isOpen ? `text-opacity-100` : `text-opacity-0`
-          }`}
-        >
+        <div className={`${isOpen ? `text-opacity-100` : `text-opacity-0 `}`}>
           <SidebarItems isOpen={isOpen} />
         </div>
       </div>
